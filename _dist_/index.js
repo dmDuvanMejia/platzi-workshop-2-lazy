87f4fa513c16f3fd746abb6f4bdc3d9cec789cf9
dmDuvanMejia <dmDuvanMejia@users.noreply.github.com>
2022-01-26 23:30:55 +0000

    deploy: 00b70f7bafcfb086ff893f78adf2fa56531851e6

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,37 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const maximun = 122;
+const minimun = 1;
+
+const random = () => { 
+    return Math.floor(Math.random() * (maximun - minimun)) + minimun;
+}
+
+
+//Crear imagenes
+
+const createImageNode = () => {
+    const container = document.createElement('div');
+    container.className = 'p-4';
+
+    const image = document.createElement('img');
+    image.className = 'mx-auto d-block';
+    image.width = '320';
+    image.src = `https://randomfox.ca/images/${random()}.jpg`;	
+
+    container.appendChild(image);
+    return container;
+}
+
+
+const createImage = createImageNode();
+
+const mountNode = document.getElementById('images');
+const addButton = document.querySelector('#btnImage');
+
+const addImage = () => { 
+    const newImage = createImageNode(); 
+    mountNode.appendChild(newImage);
+}
+addButton.addEventListener('click', addImage);
