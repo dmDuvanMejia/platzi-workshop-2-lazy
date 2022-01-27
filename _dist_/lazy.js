a1d17c85c602992810a9abf1f1e2f3dddcf6b7f2
dmDuvanMejia <dmDuvanMejia@users.noreply.github.com>
2022-01-27 15:11:06 +0000

    deploy: 1a34fc413dae5708f9ffbdf00e1572560a152efa

--- _dist_/lazy.js
+++ _dist_/lazy.js
@@ -11,6 +11,13 @@ const loadImage = ( entry ) => {
     image.src = url;
     console.log('loadImage');
 	
+    const imageWrapper = document.createElement("div");
+    imageWrapper.className = "bg-gray-300";
+    imageWrapper.style.minHeight = "100px";
+    imageWrapper.style.display = "inline-block";
+    imageWrapper.appendChild(image);
+    container.appendChild(imageWrapper);
+
     //Desregistra la imagen o deja de escuchar
     observer.unobserve(container);
 }
